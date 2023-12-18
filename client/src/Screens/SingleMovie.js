e87da4b57f60a271f83fe524e772575f3ef006ad
tientvpd06897 <105759400+tientvpd06897@users.noreply.github.com>
2023-12-18 14:17:13 +0700

    fixx

--- client/src/Screens/SingleMovie.js
+++ client/src/Screens/SingleMovie.js
@@ -57,15 +57,15 @@ function SingleMovie() {
             <MovieRates movie={movie} />
             {RelatedMovies
               ? RelatedMovies.length > 0 && (
-                  <div className="my-16">
-                    <Titles title="Related Movies" Icon={BsCollectionFill} />
-                    <div className="grid sm:mt-10 mt-6 xl:grid-cols-4 2xl:grid-cols-5 lg:grid-cols-3 sm:grid-cols-2 gap-6">
-                      {RelatedMovies.map((movie) => (
-                        <Movie key={movie?._id} movie={movie} />
-                      ))}
-                    </div>
+                <div className="my-16">
+                  <Titles title="Related Movies" Icon={BsCollectionFill} />
+                  <div className="grid sm:mt-10 mt-6 xl:grid-cols-4 2xl:grid-cols-5 lg:grid-cols-3 sm:grid-cols-2 gap-6">
+                    {RelatedMovies.map((movie) => (
+                      <Movie key={movie?._id} movie={movie} />
+                    ))}
                   </div>
-                )
+                </div>
+              )
               : ""}
           </div>
         </>
